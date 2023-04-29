c81d4857e4f284d736c83cbe8f5175e223e810c8
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-29 22:29:50 +0300

    add add button component

---
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/pizzaSlice";
+// import { Button } from '@mui/material';
+import Button from "../Button/Button";
+import { Box } from "@mui/material";
+import s from "./AddToCartButton.module.css";
+
+function AddToCartButton({ id }) {
+  const dispatch = useDispatch();
+  const add = () => dispatch(addToCart(id));
+  return (
+    <Box className={s.addToCartBox}>
+      <Button onClick={add}>Add to cart</Button>
+    </Box>
+  );
+}
+
+export default AddToCartButton;
