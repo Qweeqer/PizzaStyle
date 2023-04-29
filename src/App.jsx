5b89e1d3d1ddd8dabe3f6e0688900f42b0a93d8a
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-29 23:09:57 +0300

    edit route

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import ButtonAppBar from "./components/AppBarWithCartIcon/AppBarWithCartIcon";
+import Pizzas from "./page/Pizza/Pizza.jsx";
+import Cart from "./page/Cart/Cart.jsx";
+import PageNotFound from "./page/PageNotFound/PageNotFound.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<ButtonAppBar />}>
+        <Route index exact="true" element={<Pizzas />} />
+        <Route path="/PizzaStyle/cart" element={<Cart />} />
+      </Route>
+      <Route path="/PizzaStyle/*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
