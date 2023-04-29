124df9156f6c646f9103150697822548ff868059
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-30 01:39:34 +0300

    edit routes and 404 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import { Suspense } from "react";
 import { Loader } from "./components/Loader/Loader";
 import ButtonAppBar from "./components/AppBarWithCartIcon/AppBarWithCartIcon";
 import Pizzas from "./page/Pizza/Pizza.jsx";
 import Cart from "./page/Cart/Cart.jsx";
-import PageNotFound from "./page/PageNotFound/PageNotFound.jsx";
-import RedirectHash from "./components/RedirectHash/RedirectHash";
+// import PageNotFound from "./page/PageNotFound/PageNotFound.jsx";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const redirectPath = sessionStorage.getItem("redirect");
+    if (redirectPath) {
+      sessionStorage.removeItem("redirect");
+      window.history.replaceState(null, "", redirectPath);
+    }
+  }, []);
+
   return (
-    <BrowserRouter basename="/PizzaStyle">
-      <RedirectHash />
+    <BrowserRouter basename="/PizzaStyle/">
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<ButtonAppBar />}>
             <Route index element={<Pizzas />} />
             <Route path="/cart" element={<Cart />} />
           </Route>
-          <Route path="*" element={<PageNotFound />} />
+          <Route path="*" element={<Navigate to="/PizzaStyle/" />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
