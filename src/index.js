f75dc5766a1bde14c056ae9cef1a1c9ae8802536
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-29 22:26:59 +0300

    Edit app file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'App';
 import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="PizzaStyle">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
