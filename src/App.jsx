8c07f1fc9715c7bea7df7d4078480fbeee0e0951
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-29 23:24:22 +0300

    change routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { Loader } from "./components/Loader/Loader";
 import ButtonAppBar from "./components/AppBarWithCartIcon/AppBarWithCartIcon";
 import Pizzas from "./page/Pizza/Pizza.jsx";
 import Cart from "./page/Cart/Cart.jsx";
 import PageNotFound from "./page/PageNotFound/PageNotFound.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ButtonAppBar />}>
-        <Route index exact="true" element={<Pizzas />} />
-        <Route path="/PizzaStyle/cart" element={<Cart />} />
-      </Route>
-      <Route path="/PizzaStyle/*" element={<PageNotFound />} />
-    </Routes>
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes path="/PizzaStyle/" element={<ButtonAppBar />}>
+          <Route index exact="true" element={<Pizzas />} />
+          <Route path="/PizzaStyle/cart" element={<Cart />} />
+          {/* <Route path="*" element={<Navigate to="/PizzaStyle/" />} /> */}
+        </Routes>
+        <Route path="*" element={<PageNotFound />} />
+      </Suspense>
+    </Router>
   );
 }
 
