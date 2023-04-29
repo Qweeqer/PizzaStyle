8676c14ec00e74adb4dea92cc4382208cb5b2156
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-29 22:30:14 +0300

    add make order component

---
+++ src/components/MakeOrder/MakeOrder.jsx
@@ -0,0 +1,43 @@
+import { makeOrder } from "../../redux/pizzaSlice";
+import { Box, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../Button/Button";
+import s from "./MakeOrder.module.css";
+export default function MakeOrder() {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.pizzaStyle.cart);
+
+  const totalSum = cart.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  );
+
+  const canMakeOrder = cart.length > 0;
+
+  const handleMakeOrder = () => {
+    if (canMakeOrder) {
+      dispatch(makeOrder());
+    }
+  };
+
+  return (
+    <Box className={s.orderBox}>
+      <Typography variant="h5">Total Sum: {totalSum} UAH</Typography>
+      <Box className={s.orderButtonBox}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleMakeOrder}
+          disabled={!canMakeOrder}
+        >
+          Place Order
+        </Button>
+        {!canMakeOrder && (
+          <Typography color="error">
+            Add items to cart to place an order.
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
+}
