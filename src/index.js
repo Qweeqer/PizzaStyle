0d2d6561adab0ac3fa0a658a458c21bef0a81e40
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-30 09:53:00 +0300

    add <PersistGate persistor={persistor}

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Loader } from './components/Loader/Loader';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      
-        <App />
+      <PersistGate persistor={persistor} loading={<Loader />}>
+      <BrowserRouter basename="/PizzaStyle">
+      <App />
+      </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import pizzaReducer from './pizzaSlice';
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, pizzaReducer);
+
 export const store = configureStore({
-  reducer: { pizzaStyle: pizzaReducer },
+  reducer: { pizzaStyle: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
