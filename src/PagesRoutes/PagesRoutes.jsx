9da492de699fcb046c5f2e1bbfa210d7475263c4
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-30 09:52:08 +0300

    add Page routes in other file

---
+++ src/PagesRoutes/PagesRoutes.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { Loader } from "../components/Loader/Loader";
+import ButtonAppBar from "../components/AppBarWithCartIcon/AppBarWithCartIcon";
+
+const Pizzas = lazy(() => import("../page/Pizza/Pizza"));
+const Cart = lazy(() => import("../page/Cart/Cart"));
+const PageNotFound = lazy(() => import("../page/PageNotFound/PageNotFound"));
+const PagesRoutes = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <ButtonAppBar />
+      <Routes>
+        <Route path="/" element={<Pizzas />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default PagesRoutes;
