4e94bfdcabd3f49fbb57d9651e344420c1222e57
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-30 11:43:51 +0300

    edit routing

--- src/PagesRoutes/PagesRoutes.jsx
+++ src/PagesRoutes/PagesRoutes.jsx
@@ -1,19 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 import { Loader } from "../components/Loader/Loader";
 import ButtonAppBar from "../components/AppBarWithCartIcon/AppBarWithCartIcon";
 
 const Pizzas = lazy(() => import("../page/Pizza/Pizza"));
 const Cart = lazy(() => import("../page/Cart/Cart"));
-// const PageNotFound = lazy(() => import("../page/PageNotFound/PageNotFound"));
+const PageNotFound = lazy(() => import("../page/PageNotFound/PageNotFound"));
 const PagesRoutes = () => {
   return (
     <Suspense fallback={<Loader />}>
       <ButtonAppBar />
       <Routes>
-        <Route path="/PizzaStyle/" element={<Pizzas />} />
-        <Route path="/PizzaStyle/cart" element={<Cart />} />
-        <Route path="*" element={<Navigate to="/PizzaStyle/" />} />
+        <Route path="/" element={<Pizzas />} />
+        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Suspense>
   );
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={<Loader />}>
-      <BrowserRouter >
+      <BrowserRouter basename="/PizzaStyle">
       <App />
       </BrowserRouter>
       </PersistGate>
