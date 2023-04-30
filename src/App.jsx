2c3515fcdf8092008bd7059494dceb9968488fff
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-30 09:53:24 +0300

    add page routes in other file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import React, { lazy, Suspense } from "react";
-import { Loader } from "./components/Loader/Loader";
-import ButtonAppBar from "./components/AppBarWithCartIcon/AppBarWithCartIcon";
+import React from "react";
+import PagesRoutes from "./PagesRoutes/PagesRoutes";
 
 import "./App.css";
 
-const Pizzas = lazy(() => import("./page/Pizza/Pizza"));
-const Cart = lazy(() => import("./page/Cart/Cart"));
-
 const App = () => {
   return (
-    <BrowserRouter basename="/PizzaStyle/">
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<ButtonAppBar />}>
-            <Route index element={<Pizzas />} />
-            <Route path="cart" element={<Cart />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <>
+      <PagesRoutes/>
+    </>
   );
 };
 
