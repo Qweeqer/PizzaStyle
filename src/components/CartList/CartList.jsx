d203656897bc065eba47c9a48d3128188495fad6
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-05-01 22:04:10 +0300

    edit images styles

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -8,37 +8,35 @@ import {
   Typography,
   Box,
 } from "@mui/material";
+import s from "./CartList.module.css";
+
 function Cart() {
   const cart = useSelector((state) => state.pizzaStyle.cart);
 
   return cart.map(({ id, title, description, price, image, quantity }) => {
     return (
       <Grid item xs={12} sm={6} md={4} key={id}>
-        <Card
-          sx={{
-            height: "100%",
-            "&:hover": { boxShadow: "0px 10px 13px -7px #000000" },
-          }}
-        >
-          <CardMedia
-            image={image}
-            alt={title}
-            title={title}
-            sx={{
-              height: 400,
-              "&:hover": { transform: "scale(1.05)" },
-            }}
-          />
-          <CardContent sx={{ height: 100 }}>
-            <Typography variant="h6" component="h3">
+        <Card className={s.card}>
+          <CardMedia className={s.imageContainer}>
+            <CardMedia
+              className={s.cardMedia}
+              image={image}
+              alt={title}
+              title={title}
+            />
+          </CardMedia>
+          <CardContent className={s.cardContent} sx={{ height: 100 }}>
+            <Typography className={s.cardTitle} variant="h6" component="h3">
               {title}
             </Typography>
-            <Typography variant="body2">{description}</Typography>
-            <Typography variant="body1">
+            <Typography className={s.description} variant="body2">
+              {description}
+            </Typography>
+            <Typography className={s.description} variant="body1">
               Total price for {quantity} pizzas: {price * quantity} UAH
             </Typography>
           </CardContent>
-          <Box>
+          <Box className={s.cardActions}>
             <Counter id={id} />
           </Box>
         </Card>
--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -12,21 +12,21 @@ import {
 import s from "./PizzaItem.module.css";
 export default function PizzaItem({ pizzas }) {
   const cart = useSelector((state) => state.pizzaStyle.cart);
-
   return (
     <>
       {pizzas.map(({ id, title, description, price, image }) => {
         const isInCart = cart.some((item) => item.id === id);
-
         return (
           <Grid item xs={12} sm={6} md={4} key={id}>
             <Card className={s.card}>
-              <CardMedia
-                className={s.cardMedia}
-                image={image}
-                alt={title}
-                title={title}
-              />
+              <CardMedia className={s.imageContainer}>
+                <CardMedia
+                  className={s.cardMedia}
+                  image={image}
+                  alt={title}
+                  title={title}
+                />
+              </CardMedia>
               <CardContent className={s.cardContent} sx={{ height: 100 }}>
                 <Typography className={s.cardTitle} variant="h6" component="h3">
                   {title}
