5049f5ce58e02515169e1fe77f064694598c40d3
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-05-04 19:49:58 +0300

    Add styles when images not loaded

--- src/components/CenteredTitleImage/CenteredTitleImage.jsx
+++ src/components/CenteredTitleImage/CenteredTitleImage.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import { Box, Typography } from "@mui/material";
 
@@ -9,19 +9,29 @@ import s from "./CenteredTitleImage.module.css";
 const CenteredTitleImage = () => {
   const heroBoxRef = useRef(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useAdjustMarginTop(heroBoxRef, "header");
 
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const backgroundStyle = isLoading
+    ? { backgroundColor: "rgba(255, 255, 255, 0.3)" }
+    : { background: `url(${backgroundImage}) center center/cover no-repeat` };
+  
   return (
-    <Box
-      ref={heroBoxRef}
-      className={s.heroBox}
-      sx={{
-        background: `url(${backgroundImage}) center center/cover no-repeat`,
-      }}
-    >
+    <Box ref={heroBoxRef} className={s.heroBox} sx={backgroundStyle}>
       <Typography className={s.heroTitle} variant="h1">
         Welcome to PizzaStyle
       </Typography>
+      <img
+        src={backgroundImage}
+        alt="Background"
+        onLoad={handleImageLoad}
+        style={{ display: "none" }}
+      />
     </Box>
   );
 };
