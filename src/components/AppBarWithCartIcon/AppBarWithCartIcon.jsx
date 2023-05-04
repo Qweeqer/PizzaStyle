aeccc80da50bd6639be2f4cfb5c04a0d7550356a
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-05-04 14:55:18 +0300

    add useEffect for windowWidth

--- src/components/AppBarWithCartIcon/AppBarWithCartIcon.jsx
+++ src/components/AppBarWithCartIcon/AppBarWithCartIcon.jsx
@@ -1,18 +1,35 @@
+import { useState, useEffect } from "react";
+
 import { useSelector } from "react-redux";
-import { AppBar, Badge, Typography, Box } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
+import { AppBar, Badge, Typography, Box } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
+
 import Logo from "../Logo/Logo";
 import s from "./AppBarWithCartIcon.module.css";
-
 
 export default function AppBarWithCartIcon() {
   const cart = useSelector((state) => state.pizzaStyle.cart);
   const cartSum = cart.reduce((acc, curr) => acc + curr.quantity, 0);
-const totalSum = cart.reduce(
-  (acc, curr) => acc + curr.price * curr.quantity,
-  0
-);
+  const totalSum = cart.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  );
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
     <>
       <AppBar
@@ -29,7 +46,6 @@ const totalSum = cart.reduce(
         <Box
           className={s.appBarBox}
           sx={{
-            width: "50%",
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
@@ -47,30 +63,38 @@ const totalSum = cart.reduce(
               PizzaStyle
             </Typography>
           </NavLink>
+          <NavLink
+            id="cart"
+            className={({ isActive }) => (isActive ? s.activeLink : s.link)}
+            to="/cart"
+            style={{ marginRight: "20px" }}
+            aria-label="Cart page"
+          >
+            <Typography>
+              <Badge
+                className={s.badge}
+                color="secondary"
+                badgeContent={cartSum}
+              >
+                <ShoppingCart className={s.cartIcon} aria-label="Cart icon" />
+                <Typography variant="p" body1="span" className={s.cartSum}>
+                  {windowWidth > 375 && (
+                    <>
+                      <Typography
+                        variant="p"
+                        body1="span"
+                        className={s.cartSumNumber}
+                      >
+                        {totalSum}
+                      </Typography>{" "}
+                      UAH
+                    </>
+                  )}
+                </Typography>
+              </Badge>
+            </Typography>
+          </NavLink>
         </Box>
-        <NavLink
-          id="cart"
-          className={({ isActive }) => (isActive ? s.activeLink : s.link)}
-          to="/cart"
-          style={{ marginRight: "20px" }}
-          aria-label="Cart page"
-        >
-          <Typography>
-            <Badge className={s.badge} color="secondary" badgeContent={cartSum}>
-              <ShoppingCart className={s.cartIcon} aria-label="Cart icon" />
-              <Typography variant="p" body1="span" className={s.cartSum}>
-                <Typography
-                  variant="p"
-                  body1="span"
-                  className={s.cartSumNumber}
-                >
-                  {totalSum}
-                </Typography>{" "}
-                UAH
-              </Typography>
-            </Badge>
-          </Typography>
-        </NavLink>
       </AppBar>
       <Outlet />
     </>
