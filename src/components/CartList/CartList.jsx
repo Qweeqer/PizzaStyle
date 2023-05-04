88e97e521eda11fafb39f4ec841acc99e6d584ce
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-05-04 13:30:01 +0300

    CartList.jsx wraps the component in a common parent so that the component does not return an array of JSX elements.

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -7,47 +7,58 @@ import {
   CardContent,
   Typography,
   Box,
+  Container,
 } from "@mui/material";
 import s from "./CartList.module.css";
 
 function Cart() {
   const cart = useSelector((state) => state.pizzaStyle.cart);
 
-  return cart.map(({ id, title, description, price, image, quantity }) => {
-    return (
-      <Grid item xs={12} sm={6} md={4} key={id}>
-        <Card className={s.card}>
-          <Box className={s.imageContainer}>
-            <CardMedia
-              loading="lazy"
-              component="img"
-              className={s.cardMedia}
-              image={image}
-              alt={title}
-              title={title}
-            />
-          </Box>
-          <Box className={s.contentCardBox}>
-            <CardContent className={s.cardContent} >
-              <Typography className={s.cardTitle} variant="h6" component="h3">
-                {title}
-              </Typography>
-              <Typography className={s.description} variant="body2">
-                {description}
-              </Typography>
-              <Typography className={s.description} variant="body1">
-                Total price for {quantity} pizzas: {price * quantity} UAH
-              </Typography>
-            </CardContent>
+  return (
+    <Container>
+      <Grid className={s.cartListGridContainer} container spacing={3}>
+        {cart.map(({ id, title, description, price, image, quantity }) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} key={id}>
+              <Card className={s.card}>
+                <Box className={s.imageContainer}>
+                  <CardMedia
+                    loading="lazy"
+                    component="img"
+                    className={s.cardMedia}
+                    image={image}
+                    alt={title}
+                    title={title}
+                  />
+                </Box>
+                <Box className={s.contentCardBox}>
+                  <CardContent className={s.cardContent}>
+                    <Typography
+                      className={s.cardTitle}
+                      variant="h6"
+                      component="h3"
+                    >
+                      {title}
+                    </Typography>
+                    <Typography className={s.description} variant="body2">
+                      {description}
+                    </Typography>
+                    <Typography className={s.description} variant="body1">
+                      Total price for {quantity} pizzas: {price * quantity} UAH
+                    </Typography>
+                  </CardContent>
 
-            <Box className={s.cardActions}>
-              <Counter id={id} />
-            </Box>
-          </Box>
-        </Card>
+                  <Box className={s.cardActions}>
+                    <Counter id={id} />
+                  </Box>
+                </Box>
+              </Card>
+            </Grid>
+          );
+        })}
       </Grid>
-    );
-  });
+    </Container>
+  );
 }
 
 export default Cart;
